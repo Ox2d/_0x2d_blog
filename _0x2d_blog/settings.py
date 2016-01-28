69b6e7d51be290110a253c943d87fabc3165a8da
huangshixu <18922212172@189.cn>
2016-01-28 11:04:55 +0800

    update database config

--- _0x2d_blog/settings.py
+++ _0x2d_blog/settings.py
@@ -9,15 +9,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import jdbc
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '0x2d_blog',                      # Or path to database file if using sqlite3.
+        'ENGINE': jdbc.ENGINE, # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': jdbc.NAME,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3307',                      # Set to empty string for default.
+        'USER': jdbc.USER,
+        'PASSWORD': jdbc.PASSWORD,
+        'HOST': jdbc.HOST,                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': jdbc.PORT,                      # Set to empty string for default.
     }
 }
 
---
+++ jdbc.py
@@ -0,0 +1,6 @@
+ENGINE = 'django.db.backends.mysql'
+NAME = '0x2d_blog'
+USER = 'root'
+PASSWORD = '123456'
+HOST = ''
+PORT = 3307
